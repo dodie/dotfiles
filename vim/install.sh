6be1f034141cc8d7b1885b52b4438d95ef3fcf81
dodie <dodiehun@gmail.com>
2015-11-07 20:30:41 +0100

    Add vim setup script.

---
+++ vim/install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/dodie/vim-monokai-refined.git ~/.vim/bundle/vim-monokai-refined
+vim +PluginInstall +qall
+
