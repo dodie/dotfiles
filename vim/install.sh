29bcf59f32dc3f4d9b1151415365002463430769
Dávid Csákvári <dodiehun@gmail.com>
2017-01-03 00:35:16 +0100

    add ag and ctags to install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get install silversearcher-ag
+sudo apt-get install exuberant-ctags
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/dodie/vim-monokai-refined.git ~/.vim/bundle/vim-monokai-refined
 vim +PluginInstall +qall
