7325455399edc65f80313a545069978b442aa0f3
Dávid Csákvári <dodiehun@gmail.com>
2017-01-12 19:59:46 +0100

    Add fzf

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,5 @@
 sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
