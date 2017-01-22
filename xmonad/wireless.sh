b97ed44bcbc5e127f47066fa7e4c237f75768e01
Dávid Csákvári <dodiehun@gmail.com>
2017-01-22 17:44:44 +0100

    Fix wireless detection script.

--- xmonad/wireless.sh
+++ xmonad/wireless.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-iwconfig wlan0 2>&1 | grep -q no\ wireless\ extensions\. && {
+wlanid=`iwconfig 2>&1 | grep IEEE | awk '{print $1}'`
+
+iwconfig $wlanid 2>&1 | grep -q no\ wireless\ extensions\. && {
   echo Wired
   exit 0
 }
 
-essid=`iwconfig wlan0 | awk -F '"' '/ESSID/ {print $2}'`
-stngth=`iwconfig wlan0 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
+essid=`iwconfig $wlanid | awk -F '"' '/ESSID/ {print $2}'`
+stngth=`iwconfig $wlanid | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
 bars=`expr $stngth / 10`
 
 case $bars in
