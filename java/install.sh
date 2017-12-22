b015005b1795eb46521c49b3a587efcfaac90a97
Dávid Csákvári <dodiehun@gmail.com>
2017-12-22 23:11:09 +0100

    Bump java version

--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java9-installer
 sudo apt-get install maven
