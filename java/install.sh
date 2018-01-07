b11d553adbb2b4e85d628ffcdcb6aaa758cf40cb
Dávid Csákvári <dodiehun@gmail.com>
2018-01-07 10:13:50 +0100

    Add helpful comments

--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,8 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java9-installer
+sudo apt-get install oracle-java8-installer
+#sudo apt-get install oracle-java9-installer
 sudo apt-get install maven
+
+#sudo update-alternatives --config java
+#sudo update-alternatives --config javac
