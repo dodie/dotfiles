3de2c95bb31fcf315cd9dffbfef20466f30d34e6
Dávid Csákvári <dodiehun@gmail.com>
2019-01-27 10:54:42 +0100

    Set Mod+L to lock screen

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -37,6 +37,7 @@ main = do
 	, mouseBindings = myMouseBindings
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((mod4Mask, xK_l), spawn "xscreensaver-command -lock")
         , ((mod4Mask .|. shiftMask, xK_t), sendMessage ToggleStruts)
         , ((mod4Mask .|. shiftMask, xK_d), spawn "gsimplecal")
         , ((mod4Mask .|. shiftMask, xK_o), spawn "amixer -D pulse sset Master 5%+")
