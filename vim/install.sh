7ca168f8db4378a95b062ab2fe1992d96dad5388
Dávid Csákvári <david.csakvari@adnovum.hu>
2022-02-19 12:35:00 +0100

    Updates

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,19 @@
 sudo apt-get update
 sudo apt-get install vim-gtk
-sudo apt-get install silversearcher-ag
 sudo apt-get install exuberant-ctags
-sudo apt-get -y install python-pip
-pip install websocket-client sexpdata
+#sudo apt-get -y install python-pip
+#pip install websocket-client sexpdata
+
+sudo apt-get install silversearcher-ag
+# If the above fails, build it from source:
+# sudo yum install -y pcre-devel
+# sudo yum install xz-devel
+# cd temp
+# git clone https://github.com/ggreer/the_silver_searcher.git
+# cd the_silver_searcher
+# ./build.sh
+# sudo make install
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/dodie/vim-monokai-refined.git ~/.vim/bundle/vim-monokai-refined
 vim +PluginInstall +qall
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,15 @@
 sudo apt-get install zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#sudo yum -y install zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# If this command fails because of the following error:
+# chsh: user "<user>" does not exist
+# Then it might be because your user is managed by LDAP, and have no local entry in /etc/passwd
+# To solve it:
+#
+# sudo vim /etc/sssd/sssd.conf
+# 
+# And add this line: override_shell = /usr/bin/zsh
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
