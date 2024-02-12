1f5921fc71ffe9fab2c561e2c71cf1e450ee54cf
Dávid Csákvári <dodiehun@gmail.com>
2024-02-12 10:22:11 +0100

    Use asdfvm

--- java/install.sh
+++
@@ -1,8 +0,0 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-#sudo apt-get install oracle-java9-installer
-sudo apt-get install maven
-
-#sudo update-alternatives --config java
-#sudo update-alternatives --config javac
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,8 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install
