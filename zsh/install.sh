3a6fef9710123b32f80ccfc6b3f2296d2c9019de
Dávid Csákvári <dodiehun@gmail.com>
2024-05-19 23:57:20 +0200

    Add fzf install steps

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,6 +4,13 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+git clone https://github.com/ggreer/the_silver_searcher.git ~/.the_silver_searcher
+(
+cd .the_silver_searcher
+./build.sh
+make install
+)
+
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
