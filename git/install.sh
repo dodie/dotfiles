80840ac191d0a9eddee2825767f41137b778248d
Dávid Csákvári <dodiehun@gmail.com>
2024-07-19 23:31:53 +0200

    Add difftastic as a diffing tool

---
+++ git/install.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/bin
+cd ~/bin
+wget https://github.com/Wilfred/difftastic/releases/download/0.58.0/difft-x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf difft-x86_64-unknown-linux-gnu.tar.gz
+rm difft-x86_64-unknown-linux-gnu.tar.gz
