e6420f79308d610c666e09a3415c99668c146d48
Dávid Csákvári <dodiehun@gmail.com>
2024-09-13 14:20:59 +0200

    Copy to sys clipboard

--- nvim/nvim.configlink/lua/mappings.lua
+++ nvim/nvim.configlink/lua/mappings.lua
@@ -87,6 +87,18 @@ map('n', '<F6>', "<cmd>Trouble lsp toggle focus=false win.position=bottom<cr>", 
 -- map('n', '<leader>xQ', "<cmd>Trouble qflist toggle<cr>") -- Quickfix List
 
 
+-- Copy to clipboard
+vim.api.nvim_set_keymap('v', '<leader>y', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"*yg_', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>yy', '"*yy', { noremap = true, silent = true })
+
+-- Paste from clipboard
+vim.api.nvim_set_keymap('n', '<leader>p', '"*p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '"*P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"*p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>P', '"*P', { noremap = true, silent = true })
+
 -- Disable Help page
 vim.api.nvim_set_keymap('n', '<F1>', ':echo<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<F1>', '<C-o>:echo<CR>', { noremap = true, silent = true })
