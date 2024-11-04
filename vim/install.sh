815469f42512c9a2abf884e7aecc426003f22540
Dávid Csákvári <dodiehun@gmail.com>
2024-11-04 14:53:57 +0100

    Remove outdated install scripts

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,3 @@
-sudo apt-get update
-sudo apt-get install vim-gtk
-sudo apt-get install silversearcher-ag
-sudo apt-get install exuberant-ctags
-sudo apt-get -y install python-pip
-pip install websocket-client sexpdata
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/dodie/vim-monokai-refined.git ~/.vim/bundle/vim-monokai-refined
 vim +PluginInstall +qall
