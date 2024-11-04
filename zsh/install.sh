12707c48fef803146e5a5219e4dd91453e304e3e
Dávid Csákvári <dodiehun@gmail.com>
2024-11-04 14:54:45 +0100

    Add hint on how to change the default shell for LDAP user

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,13 @@
 sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# The setup script issues chsh to change the default shell to ZSH. This may fail if your user is managed via LDAP.
+# To change the shell in such cases, do the following:
+# sudo vi /etc/sssd/sssd.conf
+# Add the following line to the [domain/default] section:
+# override_shell = /usr/bin/zsh
+# Reboot
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
