ee264465fff2ace623a497f8b7a8de23b6331f56
David Csakvari <david.csakvari@adnovum.hu>
2024-11-05 22:15:28 +0100

    Add nvim install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,1 +1,9 @@
-#https://medium.com/@troelslenda/make-neovim-your-default-vim-the-right-way-73396c3570cd
+yum install xclip
+curl -L https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz -o ~/Downloads/nvim-linux64.tar.gz
+tar -xvzf ~/Downloads/nvim-linux64.tar.gz -C ~/Programs/
+ln -s ~/Programs/nvim-linux64/bin/nvim ~/bin/nvim
+
+# In case stuff is not working, do an update of plugins:
+# Run the `Lazy sync` command in nvim then delete the caches:
+# rm -rf .local/share/nvim
+# rm -rf .cache/nvim
