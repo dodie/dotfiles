fc49f80f3021ec3b7df6c55f16ab02ae36fb7bb9
Dávid Csákvári <dodiehun@gmail.com>
2025-05-30 14:04:37 +0200

    Show hidden files by default in nvimtree

--- nvim/nvim.configlink/lua/plugins/init.lua
+++ nvim/nvim.configlink/lua/plugins/init.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      filters = {
+        git_ignored = false,
+      },
+    },
+  },
+  {
     "folke/trouble.nvim",
     opts = {},
     cmd = "Trouble",
