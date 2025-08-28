7d17e620921c80aae06f2a1d9adf199026d7a766
Dávid Csákvári <dodiehun@gmail.com>
2025-08-28 22:37:09 +0200

    Add mergiraf

--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,10 @@
 mkdir -p ~/bin
 cd ~/bin
+
 wget https://github.com/Wilfred/difftastic/releases/download/0.58.0/difft-x86_64-unknown-linux-gnu.tar.gz
 tar -xvzf difft-x86_64-unknown-linux-gnu.tar.gz
 rm difft-x86_64-unknown-linux-gnu.tar.gz
+
+wget https://codeberg.org/mergiraf/mergiraf/releases/download/v0.13.0/mergiraf_x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf mergiraf_x86_64-unknown-linux-gnu.tar.gz
+rm mergiraf_x86_64-unknown-linux-gnu.tar.gz
